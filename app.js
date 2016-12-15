42cd0dbd8676e817db398f4cc8cb8469776c48cd
Marcel <marcel@abrasivodigital.com.br>
2016-12-15 14:09:48 -0300

    mudanças

--- app.js
+++ app.js
@@ -3,12 +3,17 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'firebase',  
+  'firebase',
+  'ngMaterial',  
   'myApp.view1',
   'myApp.view2'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+config(['$locationProvider', '$routeProvider', '$mdThemingProvider', function($locationProvider, $routeProvider, $mdThemingProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
+  $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+  $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+  $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+  $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();  
 }]);
